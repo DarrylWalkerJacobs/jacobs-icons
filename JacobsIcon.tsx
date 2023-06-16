86a1cb20182c73888d7217fcf4c38772c51b37c9
robert.fyfe <robert.fyfe@jacobs.com>
2023-06-16 11:25:35 +0100

    Changing function for svg export

--- JacobsIcon.tsx
+++ JacobsIcon.tsx
@@ -3,23 +3,27 @@ import React from "react";
 import { ReactComponent as JbsArrowRed } from "./icons/jbs-arrow-red.svg";
 import { ReactComponent as JbsArrowAmber } from "./icons/jbs-arrow-amber.svg";
 import { ReactComponent as JbsArrowGreen } from "./icons/jbs-arrow-green.svg";
+// Import other SVG components...
+
+const iconMapping = {
+  JbsArrowRed,
+  JbsArrowAmber,
+  JbsArrowGreen,
+};
 
 interface Props {
   icon: string;
   color?: string;
 }
 
-const JacobsIcon: React.FC<Props> = ({ icon, color }) => {
-  switch (icon) {
-    case "JbsArrowRed":
-      return <JbsArrowRed fill={color} />;
-    case "JbsArrowAmber":
-      return <JbsArrowAmber fill={color} />;
-    case "JbsArrowGreen":
-      return <JbsArrowGreen fill={color} />;
-    default:
-      return <div>No icon found</div>;
+const IconComponent: React.FC<Props> = ({ icon, color }) => {
+  const Icon = iconMapping[icon];
+
+  if (Icon) {
+    return <Icon fill={color} />;
   }
+
+  return <div>No icon found</div>;
 };
 
-export default JacobsIcon;
+export default IconComponent;
