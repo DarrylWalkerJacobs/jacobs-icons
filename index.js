399716a851e3c0d4db488274a9b74d804c05df94
robert.fyfe <robert.fyfe@jacobs.com>
2023-06-16 14:12:07 +0100

    updating index.js  with imports and exports

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
 import JbsArrowRed from "./icons/jbs-arrow-red.svg";
 import JbsArrowAmber from "./icons/jbs-arrow-amber.svg";
 import JbsArrowGreen from "./icons/jbs-arrow-green.svg";
-// ... import the rest of your SVG icons
+import JbsArrowDoubleHead from "./icons/jbs-arrow-double-head.svg";
+import JbsArrowDown from "./icons/jbs-arrow-down.svg";
+import JbsArrowUp from "./icons/jbs-arrow-up.svg";
+import JbsArrowOpen from "./icons/jbs-arrow-open.svg";
+import JbsBaseline from "./icons/jbs-baseline.svg";
+import JbsCalendarForm from "./icons/jbs-calendar-form.svg";
+import JbsCancel from "./icons/jbs-cancel.svg";
+import JbsCollapseNav from "./icons/jbs-collapse-nav.svg";
+import JbsDiamondLineFill from "./icons/jbs-diamond-line-fill.svg";
+import JbsExpand from "./icons/jbs-expand.svg";
+import JbsFilters from "./icons/jbs-filters.svg";
+import JbsFitAllTasks from "./icons/jbs-fit-all-tasks.svg";
+import JbsInfoCircleOutline from "./icons/jbs-info-circle-outline.svg";
+import JbsMilestones from "./icons/jbs-milestones.svg";
+import JbsOverflowMenu from "./icons/jbs-overflow-menu.svg";
+import JbsPercentage from "./icons/jbs-percentage.svg";
+import JbsPlus from "./icons/jbs-plus.svg";
+import JbsProfile from "./icons/jbs-profile.svg";
+import JbsQuestionCircleOutline from "./icons/jbs-question-circle-outline.svg";
+import JbsSearch from "./icons/jbs-search.svg";
+import JbsSettings from "./icons/jbs-settings.svg";
+import JbsTick from "./icons/jbs-tick.svg";
+import JbsToggleDown from "./icons/jbs-toggle-down.svg";
+import JbsToggleUp from "./icons/jbs-toggle-up.svg";
+import JbsZoomIn from "./icons/jbs-zoom-in.svg";
+import JbsZoomOut from "./icons/jbs-zoom-out.svg";
+import JbsZoomToToday from "./icons/jbs-zoom-to-today.svg";
 
 export {
   JbsArrowRed,
   JbsArrowAmber,
   JbsArrowGreen,
-  // ... include the rest of your SVG icons
+  JbsArrowDoubleHead,
+  JbsArrowDown,
+  JbsArrowUp,
+  JbsArrowOpen,
+  JbsBaseline,
+  JbsCalendarForm,
+  JbsCancel,
+  JbsCollapseNav,
+  JbsDiamondLineFill,
+  JbsExpand,
+  JbsFilters,
+  JbsFitAllTasks,
+  JbsInfoCircleOutline,
+  JbsMilestones,
+  JbsOverflowMenu,
+  JbsPercentage,
+  JbsPlus,
+  JbsProfile,
+  JbsQuestionCircleOutline,
+  JbsSearch,
+  JbsSettings,
+  JbsTick,
+  JbsToggleDown,
+  JbsToggleUp,
+  JbsZoomIn,
+  JbsZoomOut,
+  JbsZoomToToday,
 };
